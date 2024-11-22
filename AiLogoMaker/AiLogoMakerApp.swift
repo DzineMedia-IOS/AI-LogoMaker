7385618c78e2ac7a48fe353762f1cb0cd39cd1a5
Junaidatdzine <109285105+Junaidatdzine@users.noreply.github.com>
2024-11-22 14:32:35 +0500

    -init

---
+++ AiLogoMaker/AiLogoMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiLogoMakerApp.swift
+//  AiLogoMaker
+//
+//  Created by MacBook on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct AiLogoMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
