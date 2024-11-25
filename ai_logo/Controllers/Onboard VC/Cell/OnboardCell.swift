0753b52dfaca9dda40bb4fab90df5ea38356cdcd
ahmaddzine <ahmad.hassan@dzinemedia.com>
2024-11-25 15:10:25 +0500

    first commit

---
+++ ai_logo/Controllers/Onboard VC/Cell/OnboardCell.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardCell.swift
+//  ai_logo
+//
+//  Created by LAP__TECH on 11/25/24.
+//
+
+import UIKit
+
+class OnboardCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ ai_logo/Controllers/Onboard VC/OnboardViewController.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardViewController.swift
+//  ai_logo
+//
+//  Created by LAP__TECH on 11/25/24.
+//
+
+import UIKit
+
+class OnboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+}
