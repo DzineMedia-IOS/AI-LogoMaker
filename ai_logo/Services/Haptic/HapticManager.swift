bcf6efc89b8ee0d2b91109a321d5d0191052da0d
ahmaddzine <ahmad.hassan@dzinemedia.com>
2024-12-11 13:01:00 +0500

    haptic, trynowprompt VC

---
+++ ai_logo/Services/Haptic/HapticManager.swift
@@ -0,0 +1,42 @@
+//
+//  HapticManager.swift
+//  ai_logo
+//
+//  Created by LAP__TECH on 12/11/24.
+//
+
+
+import UIKit
+
+class HapticManager {
+    static func triggerHapticFeedback(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.prepare()
+        generator.impactOccurred()
+    }
+}
+
+// usage
+//HapticManager.triggerHapticFeedback(style: .light)
+
+
+
+func isHapticFeedbackEnabled() -> Bool {
+    return UserDefaults.standard.bool(forKey: hapticFeedbackKey)
+}
+
+extension UIButton {
+    open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        if UserDefaults.standard.bool(forKey: hapticFeedbackKey) {
+            HapticManager.triggerHapticFeedback(style: .medium)
+        }
+    }
+}
+
+func hapticFeedBackAction() {
+    if UserDefaults.standard.bool(forKey: hapticFeedbackKey) {
+               HapticManager.triggerHapticFeedback(style: .medium)
+           }
+}
+
